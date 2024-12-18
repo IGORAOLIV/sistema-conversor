56e259cda4123d46137b023549607e736dff6c9d
GuiSCampos2004 <guilherme.fields2004@gmail.com>
2024-12-18 19:37:32 -0300

    Adicionada uma funcao que converte valores em notacao cientifica

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+void notacao_cientifica(float valor){
+    int contador=0;
+    while (valor>=10){
+        contador++;
+        valor = valor/10;
+    }
+    while (valor<1){
+        contador--;
+        valor = valor*10;
+    }
+    printf("%.2fE%i", valor, contador);
+    return;
+}
+
 // Função principal
 int main() {
     printf("Bem-vindo ao Conversor de Unidades!\n");
@@ -52,11 +66,21 @@ int main() {
                     switch(final){
                         case 1:
                             valor2 = valor1*1.8 + 32;
-                            printf("%.2fºC é equivalente a %.2fºF", valor1, valor2);
+                            printf("%.2fºC é equivalente a %.2fºF\n", valor1, valor2);
+                            printf("Em notação cientifica:\n");
+                            notacao_cientifica(valor1);
+                            printf("ºC é equivalente a ");
+                            notacao_cientifica(valor2);
+                            printf("ºF");
                             break;
                         case 2:
                             valor2 = valor1+273;
-                            printf("%.2fºC é equivalente a %.2fºK", valor1, valor2);
+                            printf("%.2fºC é equivalente a %.2fºK\n", valor1, valor2);
+                            printf("Em notação cientifica:\n");
+                            notacao_cientifica(valor1);
+                            printf("ºC é equivalente a ");
+                            notacao_cientifica(valor2);
+                            printf("ºK");
                             break;
                         default:
                             printf("Opção inválida. Tente novamente.\n");
@@ -73,11 +97,21 @@ int main() {
                     switch(final){
                         case 1:
                             valor2 = (valor1-32)/1.8;
-                            printf("%.2fºF é equivalente a %.2fºC", valor1, valor2);
+                            printf("%.2fºF é equivalente a %.2fºC\n", valor1, valor2);
+                            printf("Em notação cientifica:\n");
+                            notacao_cientifica(valor1);
+                            printf("ºF é equivalente a ");
+                            notacao_cientifica(valor2);
+                            printf("ºC");
                             break;
                         case 2:
                             valor2 = (valor1-32)/1.8 + 273;
-                            printf("%.2fºF é equivalente a %.2fºK", valor1, valor2);
+                            printf("%.2fºF é equivalente a %.2fºK\n", valor1, valor2);
+                            printf("Em notação cientifica:\n");
+                            notacao_cientifica(valor1);
+                            printf("ºF é equivalente a ");
+                            notacao_cientifica(valor2);
+                            printf("ºK");
                             break;
                         default:
                             printf("Opção inválida. Tente novamente.\n");
@@ -94,11 +128,21 @@ int main() {
                     switch(final){
                         case 1:
                             valor2 = valor1 - 273;
-                            printf("%.2fºK é equivalente a %.2fºC", valor1, valor2);
+                            printf("%.2fºK é equivalente a %.2fºC\n", valor1, valor2);
+                            printf("Em notação cientifica:\n");
+                            notacao_cientifica(valor1);
+                            printf("ºK é equivalente a ");
+                            notacao_cientifica(valor2);
+                            printf("ºC");
                             break;
                         case 2:
                             valor2 = (valor1 - 273)*1.8 + 32;
-                            printf("%.2fºK é equivalente a %.2fºF", valor1, valor2);
+                            printf("%.2fºK é equivalente a %.2fºF\n", valor1, valor2);
+                            printf("Em notação cientifica:\n");
+                            notacao_cientifica(valor1);
+                            printf("ºK é equivalente a ");
+                            notacao_cientifica(valor2);
+                            printf("ºF");
                             break;
                         default:
                             printf("Opção inválida. Tente novamente.\n");
