3b9f4e03fa873c32866f85f571538fab485d3ef1
IGORAOLIV <igoralmeida0105@gmail.com>
2024-12-19 16:00:47 -0300

    Define todas as funções na interface

--- main.c
+++ main.c
@@ -7,7 +7,12 @@ int main() {
     // Exemplo de opções (a serem implementadas)
     printf("1. Converter Comprimento\n");
     printf("2. Converter Massa\n");
-    printf("3. Sair\n");
+    printf("3. Converter Volume\n");
+    printf("4. Converter Temperatura\n");
+    printf("5. Converter Velocidade\n");
+    printf("6. Converter Bits & Bytes\n");
+    printf("7. Converter Área\n");
+    printf("8. Sair\n");
 
     int opcao;
     scanf("%d", &opcao);
@@ -20,11 +25,25 @@ int main() {
             printf("Funcionalidade de massa será implementada aqui.\n");
             break;
         case 3:
+            printf("Funcionalidade de volume será implementada aqui.\n");
+            break;
+        case 4:
+            printf("Funcionalidade de temperatura será implementada aqui.\n");
+            break;
+        case 5:
+            printf("Funcionalidade de velocidade será implementada aqui.\n");
+            break;
+        case 6:
+            printf("Funcionalidade de bits & bytes será implementada aqui.\n");
+            break;
+        case 7:
+            printf("Funcionalidade de área será implementada aqui.\n");
+            break;
+        case 8:
             printf("Saindo do programa. Até mais!\n");
             return 0;
         default:
             printf("Opção inválida. Tente novamente.\n");
     }
-
     return 0;
 }
