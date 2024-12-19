9226f9d4596901b9b53c6ad0fe7eea7e23f5efdd
Gabriel Alves <gabriel.lex20@gmail.com>
2024-12-19 16:38:31 -0300

    cria escolhas iniciais do usuario

---
+++ velocidade.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "velocidade.h"
+
+
+void converterVelocidade(){
+
+    int  option;
+    float valor,resultado;
+
+    printf("Conversor de unidades de velocidade\n");
+    printf("Escolha a conversão desejada:\n");
+    printf("1. km/h para m/s\n");
+    printf("2. km/h para mph\n");
+    printf("3. m/s para km/h\n");
+    printf("4. m/s para mph\n");
+    printf("5. mph para km/h\n");
+    printf("6. mph para m/s\n");
+    printf("7. Sair\n");
+    printf("Opção: ");
+    scanf("%d", &option);
+//A operação será definida aparti da opção que você escolher de 1 a 7.
+}
