b6016ad8e5e03cbaf8f9aa713a2ea84e2e4f30dd
Gabriel Alves <gabriel.lex20@gmail.com>
2024-12-19 16:58:48 -0300

    apresentar erro caso a escolha no switch for diferente de 1 a 7

--- velocidade.c
+++ velocidade.c
@@ -28,5 +28,43 @@ void converterVelocidade(){
 //Após isso o valor será caculado
             printf("%.2f km/h = %.2f m/s\n", valor, resultado);
 //E o seu resultado será exibido aqui
-    }
+        break;
+        case 2:
+            printf("Digite o valor a ser convertido: ");
+            scanf("%f", &valor);
+            resultado = valor * 0.621371;
+            printf("%.2f km/h = %.2f mph\n", valor, resultado);
+            break;
+        case 3:
+            printf("Digite o valor a ser convertido: ");
+            scanf("%f", &valor);
+            resultado = valor * 3.6;
+            printf("%.2f m/s = %.2f km/h\n", valor, resultado);
+            break;
+        case 4:
+            printf("Digite o valor a ser convertido: ");
+            scanf("%f", &valor);
+            resultado = valor * 2.23694;
+            printf("%.2f m/s = %.2f mph\n", valor, resultado);
+            break;
+        case 5:
+            printf("Digite o valor a ser convertido: ");
+            scanf("%f", &valor);
+            resultado = valor / 0.621371;
+            printf("%.2f mph = %.2f km/h\n", valor, resultado);
+            break;
+        case 6:
+            printf("Digite o valor a ser convertido: ");
+            scanf("%f", &valor);
+            resultado = valor / 2.23694;
+            printf("%.2f mph = %.2f m/s\n", valor, resultado);
+            break;
+        case 7:
+            printf("Finalizando operação!");
+            break;    
+        default:
+            printf("Opção inválida!\n");
+//caso o valor informado na opção não for um numero de 1 a 7 ele apresentara erro!                    
+        }
+            
 }
