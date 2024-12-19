06b722975c66fb963e8821b7673dc3e405362406
GuiSCampos2004 <guilherme.fields2004@gmail.com>
2024-12-19 16:19:01 -0300

    Atualização da interface principal

--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 
 // Função principal
 int main() {
+    // Exemplo de opções (a serem implementadas)
     printf("Bem-vindo ao Conversor de Unidades!\n");
     printf("Selecione uma opção para começar:\n");
-    // Exemplo de opções (a serem implementadas)
     printf("1. Converter Comprimento\n");
     printf("2. Converter Massa\n");
     printf("3. Converter Volume\n");
     printf("4. Converter Temperatura\n");
-    printf("5. Sair\n");
+    printf("5. Converter Velocidade\n");
+    printf("6. Converter Bits & Bytes\n");
+    printf("7. Converter Área\n");
+    printf("8. Sair\n");
 
     int opcao;
     scanf("%d", &opcao);
@@ -25,17 +28,23 @@ int main() {
         case 3:
             printf("Funcionalidade de volume será implementada aqui.\n");
             break;
-
         case 4:
             converterTemperatura();
             break;
-            
         case 5:
+            printf("Funcionalidade de velocidade será implementada aqui.\n");
+            break;
+        case 6:
+            printf("Funcionalidade de bits & bytes será implementada aqui.\n");
+            break;
+        case 7:
+            printf("Funcionalidade de área será implementada aqui.\n");
+            break;
+        case 8:
             printf("Saindo do programa. Até mais!\n");
             return 0;
         default:
             printf("Opção inválida. Tente novamente.\n");
     }
-
     return 0;
 }
