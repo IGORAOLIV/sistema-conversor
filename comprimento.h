840e53094c23027035a38b209e2343f018befe19
Aulo Cezar <aulofferreira@gmail.com>
2024-12-20 20:07:13 -0300

    Define menu para o código

--- comprimento.h
+++ comprimento.h
@@ -9,6 +9,17 @@ void comprimento()
     int escolha;
     double valor;
 
+    printf("Conversor de Unidades\n");
+    printf("1. Metros para Centímetros\n");
+    printf("2. Metros para milímetros\n");
+    printf("3. Centímetros para Metros\n");
+    printf("Escolha uma conversão (1-3): ");
+    scanf("%d", &escolha);
+
+    printf("Digite o valor para converter: ");
+    scanf("%lf", &valor);
+
+
 }
 
 
