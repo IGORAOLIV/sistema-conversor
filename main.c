b82527e7a849c7347bed798988ec9d0a5ca0cac2
Aulo Cezar <aulofferreira@gmail.com>
2024-12-20 20:47:50 -0300

    Adiciona biblioteca <stdlib.h> e uso da função system('chcp 65001 > null') para alterar a pagína do console para utf-8

--- main.c
+++ main.c
@@ -1,43 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "temperatura.c"
 #include "comprimento.h"
 #include "menu.h"
 
 // Função principal
-int main() {
+int main()
+{
+
+    system("chcp 65001 > NULL");
+
     int opcao = menu();
-    while(opcao!=8){
-     
+    while (opcao != 8)
+    {
+
         scanf("%d", &opcao);
 
-        switch (opcao) {
-            case 1:
-                comprimento();
-                break;
-            case 2:
-                printf("Funcionalidade de massa será implementada aqui.\n");
-                break;
-            case 3:
-                printf("Funcionalidade de volume será implementada aqui.\n");
-                break;
-            case 4:
-                converterTemperatura();
-                break;
-            case 5:
-                printf("Funcionalidade de velocidade será implementada aqui.\n");
-                break;
-            case 6:
-                printf("Funcionalidade de bits & bytes será implementada aqui.\n");
-                break;
-            case 7:
-                printf("Funcionalidade de área será implementada aqui.\n");
-                break;
-            case 8:
-                printf("Saindo do programa. Até mais!\n");
-                break;
-            default:
-                printf("Opção inválida. Tente novamente.\n");
-            }
+        switch (opcao)
+        {
+        case 1:
+            comprimento();
+            break;
+        case 2:
+            printf("Funcionalidade de massa será implementada aqui.\n");
+            break;
+        case 3:
+            printf("Funcionalidade de volume será implementada aqui.\n");
+            break;
+        case 4:
+            converterTemperatura();
+            break;
+        case 5:
+            printf("Funcionalidade de velocidade será implementada aqui.\n");
+            break;
+        case 6:
+            printf("Funcionalidade de bits & bytes será implementada aqui.\n");
+            break;
+        case 7:
+            printf("Funcionalidade de área será implementada aqui.\n");
+            break;
+        case 8:
+            printf("Saindo do programa. Até mais!\n");
+            break;
+        default:
+            printf("Opção inválida. Tente novamente.\n");
+        }
     }
     return 0;
 }
