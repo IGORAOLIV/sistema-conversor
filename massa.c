40ee8bd0d097b5c6690634cf571fa0f08dec2886
hemerson-ufob2021 <hemerson.s3035@ufob.edu.br>
2024-12-20 15:47:09 -0300

    Update massa.c

--- massa.c
+++ massa.c
@@ -2,5 +2,13 @@
 #include "massa.h"
 
 void converterMassa() {
+
+    printf("Selecione a unidade inicial:\n");
+    printf("1. Grama/n");
+    printf("2. Quilograma/n");
+    printf("3. Tonelada/n");
+
+    int inicial, final;
+    scanf("%d"
     
 }
