373261b4303e063983e2ddb4cfb1d59819330902
hemerson-ufob2021 <hemerson.s3035@ufob.edu.br>
2024-12-20 16:25:02 -0300

    Update massa.c

--- massa.c
+++ massa.c
@@ -9,6 +9,21 @@ void converterMassa() {
     printf("3. Tonelada/n");
 
     int inicial, final;
-    scanf("%d"
-    
+    scanf("%d", &inicial);
+
+    printf("Digite o valor: ");
+    float valor1, valor2;
+    scanf("%f", &valor1);
+
+    printf("Escolha a unidade que deseja:\n");
+    switch(inicial){
+        case 1:
+            printf("1. Quilograma\n");
+            printf("2. Tonelada\n");
+
+            scanf("%d", &final);
+
+            switch(final){
+
+                case 1:
 }
