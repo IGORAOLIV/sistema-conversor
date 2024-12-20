9de10b1c8ef9cf4fe1c6687b2a1d346f9271c840
Aulo Cezar <aulofferreira@gmail.com>
2024-12-20 20:33:37 -0300

    Cria prints para ser exibido para o usuário e retorna o valor da escolha para a função principal

--- menu.h
+++ menu.h
@@ -9,6 +9,20 @@ int menu()
     int escolha;
 
     system("cls");
+
+    printf("Bem-vindo ao Conversor de Unidades!\n");
+    printf("Selecione uma opção para começar:\n");
+    printf("1. Converter Comprimento\n");
+    printf("2. Converter Massa\n");
+    printf("3. Converter Volume\n");
+    printf("4. Converter Temperatura\n");
+    printf("5. Converter Velocidade\n");
+    printf("6. Converter Bits & Bytes\n");
+    printf("7. Converter Área\n");
+    printf("8. Sair\n");
+    printf("Sua escolha: ");
+
+    return escolha;
 }
 
 #endif
