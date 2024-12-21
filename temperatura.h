a8335e4a47203982eba17665bc2af93d49787311
GuiSCampos2004 <guilherme.fields2004@gmail.com>
2024-12-21 16:01:17 -0300

    Adicionado opção de retorno para a interface principal

--- temperatura.h
+++ temperatura.h
@@ -4,117 +4,122 @@
 
 void converterTemperatura(){    //função para converter a temperatura que o usuário dá para temperatura que o usuário quer
     //Usuário dá uma unidade de temperatura
-    printf("Selecione a unidade inicial:\n");
-    printf("1. Celsius\n");
-    printf("2. Fahrenheit\n");
-    printf("3. Kelvin\n");
+    int loop1 = 1;
+    while(loop1){
+        printf("Selecione a unidade inicial:\n");
+        printf("1. Celsius\n");
+        printf("2. Fahrenheit\n");
+        printf("3. Kelvin\n");
+        printf("4. Voltar\n");
 
-    int inicial, final;
-    scanf("%d", &inicial);
+        int inicial, final;
+        scanf("%d", &inicial);
 
-    float valor1, valor2;
-    switch(inicial){
-        case 1:
-            //-----------------------------------------------
-            //Usuário indica o valor
-            printf("Agora digite o valor: ");
-            scanf("%f", &valor1);
+        float valor1, valor2;
+        switch(inicial){
+            case 1:
+                //-----------------------------------------------
+                //Usuário indica o valor
+                printf("Agora digite o valor: ");
+                scanf("%f", &valor1);
 
-            //-----------------------------------------------
-            //A partir daqui o usuário escolhe qual a unidade que deseja
-            printf("Agora escolha a unidade que deseja:\n");
-            printf("1. Fahrenheit\n");
-            printf("2. Kelvin\n");
-            scanf("%d", &final);
+                //-----------------------------------------------
+                //A partir daqui o usuário escolhe qual a unidade que deseja
+                printf("Agora escolha a unidade que deseja:\n");
+                printf("1. Fahrenheit\n");
+                printf("2. Kelvin\n");
+                scanf("%d", &final);
 
-            switch(final){
-                //Conversão Celsius para...
-                case 1:
-                    //...Fahrenheit
-                    valor2 = valor1 * 1.8 + 32;
-                    printf("%.2fºC é equivalente a %.2fºF\n", valor1, valor2);
-                    break;
+                switch(final){
+                    //Conversão Celsius para...
+                    case 1:
+                        //...Fahrenheit
+                        valor2 = valor1 * 1.8 + 32;
+                        printf("%.2fºC é equivalente a %.2fºF\n", valor1, valor2);
+                        break;
 
-                case 2:
-                    //...Kelvin
-                    valor2 = valor1 + 273;
-                    printf("%.2fºC é equivalente a %.2fºK\n", valor1, valor2);
-                    break;
+                    case 2:
+                        //...Kelvin
+                        valor2 = valor1 + 273;
+                        printf("%.2fºC é equivalente a %.2fºK\n", valor1, valor2);
+                        break;
                         
-                default:
-                    printf("Opção inválida. Tente novamente.\n");
-            }
-            break;
+                    default:
+                        printf("Opção inválida. Tente novamente.\n");
+                }
+                break;
 
-        case 2:
-            //-----------------------------------------------
-            //Usuário indica o valor
-            printf("Agora digite o valor: ");
-            scanf("%f", &valor1);
+            case 2:
+                //-----------------------------------------------
+                //Usuário indica o valor
+                printf("Agora digite o valor: ");
+                scanf("%f", &valor1);
 
-            //-----------------------------------------------
-            //A partir daqui o usuário escolhe qual a unidade que deseja
-            printf("Agora escolha a unidade que deseja:\n");
-            printf("1. Celsius\n");
-            printf("2. Kelvin\n");
-            scanf("%d", &final);
+                //-----------------------------------------------
+                //A partir daqui o usuário escolhe qual a unidade que deseja
+                printf("Agora escolha a unidade que deseja:\n");
+                printf("1. Celsius\n");
+                printf("2. Kelvin\n");
+                scanf("%d", &final);
 
-            switch(final){
-                //Conversão Fahrenheit para...
-                case 1:
-                    //...Celsius
-                    valor2 = (valor1-32)/1.8;
-                    printf("%.2fºF é equivalente a %.2fºC\n", valor1, valor2);
-                    break;
+                switch(final){
+                    //Conversão Fahrenheit para...
+                    case 1:
+                        //...Celsius
+                        valor2 = (valor1-32)/1.8;
+                        printf("%.2fºF é equivalente a %.2fºC\n", valor1, valor2);
+                        break;
 
-                case 2:
-                    //...Kelvin
-                    valor2 = (valor1-32)/1.8 + 273;
-                    printf("%.2fºF é equivalente a %.2fºK\n", valor1, valor2);
-                    break;
+                    case 2:
+                        //...Kelvin
+                        valor2 = (valor1-32)/1.8 + 273;
+                        printf("%.2fºF é equivalente a %.2fºK\n", valor1, valor2);
+                        break;
                         
-                default:
-                    printf("Opção inválida. Tente novamente.\n");
-            }
-            break;
+                    default:
+                        printf("Opção inválida. Tente novamente.\n");
+                }
+                break;
                 
-        case 3:
-            //-----------------------------------------------
-            //Usuário indica o valor
-            printf("Agora digite o valor: ");
-            scanf("%f", &valor1);
+            case 3:
+                //-----------------------------------------------
+                //Usuário indica o valor
+                printf("Agora digite o valor: ");
+                scanf("%f", &valor1);
 
-            //-----------------------------------------------
-            //A partir daqui o usuário escolhe qual a unidade que deseja
-            printf("Agora escolha a unidade que deseja:\n");
-            printf("1. Celsius\n");
-            printf("2. Fahrenheit\n");
+                //-----------------------------------------------
+                //A partir daqui o usuário escolhe qual a unidade que deseja
+                printf("Agora escolha a unidade que deseja:\n");
+                printf("1. Celsius\n");
+                printf("2. Fahrenheit\n");
 
-            scanf("%d", &final);
+                scanf("%d", &final);
 
-            switch(final){
-                //Conversão Kelvin para...
-                case 1:
-                    //...Celsius
-                    valor2 = valor1 - 273;
-                    printf("%.2fºK é equivalente a %.2fºC\n", valor1, valor2);
-                    break;
+                switch(final){
+                    //Conversão Kelvin para...
+                    case 1:
+                        //...Celsius
+                        valor2 = valor1 - 273;
+                        printf("%.2fºK é equivalente a %.2fºC\n", valor1, valor2);
+                        break;
 
-                case 2:
-                    //...Fahrenheit
-                    valor2 = (valor1 - 273)*1.8 + 32;
-                    printf("%.2fºK é equivalente a %.2fºF\n", valor1, valor2);
-                    break;
+                    case 2:
+                        //...Fahrenheit
+                        valor2 = (valor1 - 273)*1.8 + 32;
+                        printf("%.2fºK é equivalente a %.2fºF\n", valor1, valor2);
+                        break;
                         
-                default:
-                    printf("Opção inválida. Tente novamente.\n");
-            }
-            break;
-
-        default:
-            printf("Opção inválida. Tente novamente.\n");
+                    default:
+                        printf("Opção inválida. Tente novamente.\n");
+                }
+                break;
+            case 4:
+                loop1 = 0;
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
     }
-    printf("\n\n");
     return;
 }
 
