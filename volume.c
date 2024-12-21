c5e6a7e3c9ae8d1232236715926b817718fa2aaf
Joscielle <josciellejb@gmail.com>
2024-12-21 15:08:51 -0300

    função que converte litro para metros cubicos

---
+++ volume.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Função que converte litro para mililitro
+
+double litro_para_mililitros(double litros) {
+    return litros * 1000.0;
+}
+//Função que converte litros para metodos cubicos
+
+double litros_para_metrosCubicos(double litros) {
+    return litros / 1000.0;
+}
