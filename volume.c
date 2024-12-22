78cde74851e27993c07613dec7f0c54d3d13e23e
Joscielle <josciellejb@gmail.com>
2024-12-22 14:20:14 -0300

    conversões de metros cubicos

--- volume.c
+++ volume.c
@@ -16,3 +16,21 @@ double litros_para_metrosCubicos(double litros) {
 double mililitros_para_litros(double mililitros) {
     return mililitros / 1000.0;
 }
+
+//Função que converte mililitros em metros cubicos
+
+double mililitros_para_metros_cubicos(double mililitros) {
+    return mililitros / 1000000.0;
+}
+
+//Função para converter metros cubicos para litros
+
+double metros_cubicos_para_litros(double metros_cubicos) {
+    return metros_cubicos * 1000.0;
+}
+
+//Função para converter metros cubicos em mililitros
+
+double metros_cubicos_para_mililitros(double metros_cubicos) {
+    return metros_cubicos * 1000000.0;
+}
