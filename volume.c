f7644472c735900d14ba6d4f0f77f58b0016595e
Joscielle <josciellejb@gmail.com>
2024-12-22 15:58:32 -0300

    leitura de opcoes

--- volume.c
+++ volume.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "volume.h"
 
-//Função para converter a Volume inserido pelo usuário para Volume requerido
+//Menu para solicitação de escolha de Volume indicado para conversão do Volume escolhido
 
 void converterVolume(){
     printf("=============== Conversor de Volume ===============\n");
@@ -12,4 +12,15 @@ void converterVolume(){
     printf("4. Mililitros para Metros Cubicos\n");
     printf("5. Metros Cubicos para Litros\n");
     printf("6. Metros cubicos para Mililitros\n");
+
+    //Leitura da opção do usuario
+    int opcao;
+    scanf("%d", &opcao);
+
+    //Solicitação dos valores para conversao e leitura do valor a ser convertido
+    printf("Digite o valor a ser convertido: ");
+    double valor1, valor2;
+    scanf("%lf", &valor1);
+    
+
 }
