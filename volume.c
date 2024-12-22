feeb7597644c5c74c69c8503c1fb414cac1857a3
Joscielle <josciellejb@gmail.com>
2024-12-22 15:53:34 -0300

    criação de menu de conversão

--- volume.c
+++ volume.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include "volume.h"
 
+//Função para converter a Volume inserido pelo usuário para Volume requerido
+
+void converterVolume(){
+    printf("=============== Conversor de Volume ===============\n");
+    printf("Escolha a opção correspondente a sua conversão: ");
+    printf("1. Litros para Mililitros\n");
+    printf("2. Litros para Metros Cubicos\n");
+    printf("3. Mililitros para Litros\n");
+    printf("4. Mililitros para Metros Cubicos\n");
+    printf("5. Metros Cubicos para Litros\n");
+    printf("6. Metros cubicos para Mililitros\n");
+}
