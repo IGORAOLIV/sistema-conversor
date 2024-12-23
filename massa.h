bbeb9acc6f96f766671100ee5421cb137cd1fca1
Igor Almeida <igoralmeida0105@gmail.com>
2024-12-23 16:14:03 -0300

    Corrige função massa()

--- massa.h
+++ massa.h
@@ -4,9 +4,9 @@
 void converterMassa() {
 
     printf("Selecione a unidade inicial:\n");
-    printf("1. Grama/n");
-    printf("2. Quilograma/n");
-    printf("3. Tonelada/n");
+    printf("1. Grama\n");
+    printf("2. Quilograma\n");
+    printf("3. Tonelada\n");
 
     int inicial, final;
     scanf("%d", &inicial);
